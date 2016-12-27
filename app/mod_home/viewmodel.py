e306d2043d8a8b542b8bb30d93cdf12d0d792c20
spasal <alex.spassovsimeonov@outlook.be>
2016-12-27 20:27:31 +0100

    face detection implemented

--- app/mod_home/viewmodel.py
+++ app/mod_home/viewmodel.py
@@ -1,9 +1,11 @@
 from app.models import VideoCamera, Dream, detect_faces
 from app.common import file_io
 from flask import Response
-import time, os, datetime
+import datetime
+import time
 import json
 import cv2
+import os
 
 
 class ViewModel(object):
@@ -44,7 +46,7 @@ class ViewModel(object):
 
     def get_default_control_values(self):
         data = {
-            "iteration": self.__iterations, 
+            "iteration": self.__iterations,
             "layers": self.__layers,
             "all_layers": self.__all_layers
         }
@@ -53,7 +55,7 @@ class ViewModel(object):
 
     # GET VIDEO STREAM
     def video_feed(self):
-        return Response(self.__gen(VideoCamera()), mimetype = 'multipart/x-mixed-replace; boundary=frame')
+        return Response(self.__gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
     # CORE VM DREAM LOGIC
@@ -66,10 +68,15 @@ class ViewModel(object):
             # REGULAR STREAM
             if self.__show_general:
                 frame = camera.get_frame(False)
-                has_faces, frame = detect_faces(frame)
-                frame = camera.convert_to_jpeg(frame)
-                yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+                is_slideshow, frame = detect_faces(frame)
+
+                if not is_slideshow:
+                    frame = camera.convert_to_jpeg(frame)
+                    yield (b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+                else:
+                    # put logic to show slideshow
+                    a = "a"
 
 
             # SHOW THE RESULT
@@ -90,8 +97,7 @@ class ViewModel(object):
                 frame_dream = dream_generator.render_deepdream(layer, frame, self.__iterations)
                 frame_dream = camera.convert_to_jpeg(frame_dream)
 
-                yield (b'--frame\r\n'
-                         b'Content-Type: image/jpeg\r\n\r\n' + frame_dream + b'\r\n\r\n')
+                yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame_dream + b'\r\n\r\n')
 
                 self.__frame_dream = frame_dream
                 self.__init_dream = False
@@ -106,8 +112,7 @@ class ViewModel(object):
                 resting = self.__count - elapsed
 
                 frame_text = camera.get_frame(False)
-                cv2.putText(img=frame_text, text=str(resting), org=(300,300),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=4, thickness=4, color=(255, 255, 255))
+                cv2.putText(img=frame_text, text=str(resting), org=(300, 300), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=4, thickness=4, color=(255, 255, 255))
                 frame_text = camera.convert_to_jpeg(frame_text)
 
                 yield (b'--frame\r\n'
@@ -124,7 +129,7 @@ class ViewModel(object):
     def __save_dream(self, frame_dream, layer, iterations):
         # get path and filename
         filename = str(time.time()) + ".jpg"
-        path = os.path.join(os.getcwd(), "resources", "static","uploads", "history", filename)
+        path = os.path.join(os.getcwd(), "resources", "static", "uploads", "history", filename)
 
         # save image
         file_io.save_file(path, frame_dream)
--- app/models/detect_faces.py
+++ app/models/detect_faces.py
@@ -1,20 +1,21 @@
 import cv2
 import copy
 import os
+import time
 
 # face detection cascades
 __current_dir = os.path.dirname(__file__)
-
 __relative_path = '../../resources/cascades'
 __full_path = os.path.join(__current_dir, __relative_path)
-
 __face_cascade_path = os.path.join(__full_path, 'haarcascade_frontalface_default.xml')
 __alt_face_cascade_path = os.path.join(__full_path, 'haarcascade_profileface.xml')
 __face_cascade = cv2.CascadeClassifier(__face_cascade_path)
 __alt_face_cascade = cv2.CascadeClassifier(__alt_face_cascade_path)
 
 __has_faces = []
+__latest_has_faces = []
 __count = 60
+__count_time = time.time()
 
 
 def __drawFaces(frame, faces, color):
@@ -27,29 +28,49 @@ def __drawText(frame, text):
                 fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, thickness=2, color=(255, 255, 255))
 
 
-def detect_faces(frame):
+def __append_to_arr(arr, val, count):
+    arr.append(val)
+    if len(arr) > count:
+        arr.pop(0)
 
-    # Our operations on the frame come here
+
+def detect_faces(frame):
+    # detect face
     frm = copy.deepcopy(frame)
     gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
 
     faces_front = __face_cascade.detectMultiScale(gray, 1.3, 5)
     faces_alt = __alt_face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    hasFace = False
+    has_face = False
     if(faces_front != () or faces_alt != ()):
-            # drawFaces(frame, faces_front, (255, 0, 0))
-            # drawFaces(frame, faces_alt, (0, 255, 0))
+        has_face = True
+
+    # running average of has_face
+    __append_to_arr(__has_faces, has_face, __count)
+    i_trues, i_false = __has_faces.count(True), __has_faces.count(False)
+    has_face = i_trues > i_false
+    if has_face:
         __drawText(frame, 'Deep dreams are made of you')
-        hasFace = True
 
-    __has_faces.append(hasFace)
-    if len(__has_faces) > __count:
-        __has_faces.pop(0)
+    # add result to has_face_avg
+    __append_to_arr(__latest_has_faces, has_face, 2)
 
-    i_trues = __has_faces.count(True)
-    i_false = __has_faces.count(False)
+    # check if slideshow should start
+    slideshow = False
+    if not has_face:
+        global __count_time
+        index = len(__latest_has_faces) - 1
+        last_has_face = __latest_has_faces[index - 1]
 
-    hasFace = i_trues > i_false
+        # previous frame has face, start counting
+        if last_has_face:
+            __count_time = time.time()
 
-    return hasFace, frame
+        # check if value exceeds
+        else:
+            elapsed = int(time.time() - __count_time)
+            if elapsed > 3:
+                slideshow = True
+
+    return slideshow, frame
